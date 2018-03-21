fc305029a71a0ef8d2dae2ff0cb5927ee27956ab
Jeremy Parker <jpp39@cam.ac.uk>
2018-03-21 16:06:32 +0000

    Optionally plot debug

--- Graph.h
+++ Graph.h
@@ -71,6 +71,41 @@ inline void HeatPlot1D(const Nodal1D<N1, N2, N3> &U, std::string filename)
     matplotlibcpp::close();
 }
 
+#ifdef DEBUG_PLOT
+
+template<int N1, int N2, int N3>
+inline void HeatPlot(const NodalField<N1, N2, N3> &U, stratifloat L1, stratifloat L3, int j2, std::string filename)
+{
+    matplotlibcpp::figure();
+
+    std::vector<stratifloat> imdata(N1*N3);
+
+    for (int n1=0; n1<N1; n1++)
+    {
+        for (int n3=0; n3<N3; n3++)
+        {
+            imdata[n3*N1 + n1] = U(n1,j2,n3);
+        }
+    }
+
+    matplotlibcpp::imshow(imdata, N3, N1);
+
+    matplotlibcpp::save(filename);
+    matplotlibcpp::close();
+}
+
+template<int N1, int N2, int N3>
+inline void HeatPlot(const ModalField<N1, N2, N3> &u, stratifloat L1, stratifloat L3, int j2, std::string filename)
+{
+    NodalField<N1, N2, N3> U(u.BC());
+
+    u.ToNodal(U);
+
+    HeatPlot(U, L1, L3, j2, filename);
+}
+
+#else
+
 template<int N1, int N2, int N3>
 inline void HeatPlot(const NodalField<N1, N2, N3> &U, stratifloat L1, stratifloat L3, int j2, std::string filename)
 {
@@ -113,3 +148,5 @@ inline void HeatPlot(const ModalField<N1, N2, N3> &u, stratifloat L1, stratifloa
 
     HeatPlot(U, L1, L3, j2, filename);
 }
+
+#endif
