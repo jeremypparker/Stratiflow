1e6b3b3590e2c6af380310578ccb9e28998ba50b
Jeremy Parker <jpp39@cam.ac.uk>
2018-06-04 11:08:28 +0100

    slightly better vertical derivatives/interpolation

--- Differentiation.cpp
+++ Differentiation.cpp
@@ -24,39 +24,39 @@ MatrixX VerticalSecondDerivativeMatrix(stratifloat L, int N, BoundaryCondition o
         {
             // one sided (second order) derivatives at ends
 
-            stratifloat h1 = d(0);
-            stratifloat h2 = d(1);
+            stratifloat h1 = -d(0);
+            stratifloat h2 = d(0)+d(1);
 
             stratifloat denom = (h1+h2)*h1*h2/2.0;
 
+            D(0,1) = h2/denom;
+            D(0,0) = -(h1+h2)/denom;
             D(0,2) = h1/denom;
-            D(0,1) = -(h1+h2)/denom;
-            D(0,0) = h2/denom;
         }
         else if (j==N-1)
         {
             if (originalBC == BoundaryCondition::Neumann)
             {
                 stratifloat h1 = d(N-2);
-                stratifloat h2 = d(N-3);
+                stratifloat h2 = -(d(N-2)+d(N-3));
 
                 stratifloat denom = (h1+h2)*h1*h2/2.0;
 
+                D(N-1,N-2) = h2/denom;
+                D(N-1,N-1) = -(h1+h2)/denom;
                 D(N-1,N-3) = h1/denom;
-                D(N-1,N-2) = -(h1+h2)/denom;
-                D(N-1,N-1) = h2/denom;
             }
         }
         else if (j==N-2 && originalBC == BoundaryCondition::Dirichlet)
         {
             stratifloat h1 = d(N-3);
-            stratifloat h2 = d(N-4);
+            stratifloat h2 = -(d(N-3)+d(N-4));
 
             stratifloat denom = (h1+h2)*h1*h2/2.0;
 
+            D(N-2,N-3) = h2/denom;
+            D(N-2,N-2) = -(h1+h2)/denom;
             D(N-2,N-4) = h1/denom;
-            D(N-2,N-3) = -(h1+h2)/denom;
-            D(N-2,N-2) = h2/denom;
         }
         else
         {
@@ -67,7 +67,7 @@ MatrixX VerticalSecondDerivativeMatrix(stratifloat L, int N, BoundaryCondition o
             stratifloat denom = (h1+h2)*h1*h2/2.0;
 
             D(j,j-1) = h2/denom;
-            D(j,j) = -(h1+h2)/denom;
+            D(j,j)   = -(h1+h2)/denom;
             D(j,j+1) = h1/denom;
         }
     }
@@ -100,30 +100,38 @@ MatrixX VerticalDerivativeMatrix(stratifloat L, int N, BoundaryCondition origina
 
 MatrixX VerticalReinterpolationMatrix(stratifloat L, int N, BoundaryCondition originalBC)
 {
-    // first order interpolation at one set of points of other
     MatrixX D = MatrixX::Zero(N,N);
 
     if (originalBC == BoundaryCondition::Neumann)
     {
-        D.diagonal(1).setConstant(0.5);
-        D.diagonal(0).head(N-1).setConstant(0.5);
+        // 2nd order interpolation (Note - causes some bits not to be conserving)
+        ArrayX diff = dzStaggered(L,N); // N of these
+        ArrayX diff2 = dz(L,N);         // N-1 of these
 
-        // zero at ends
-        D.row(0).setZero();
+        D.diagonal(1) = 0.5*diff.head(N-2)/diff2;
+        D.diagonal().head(N-1) = 0.5*diff.tail(N-2)/diff2;
+
+        // values at ends interpolate exactly
+        D(0,0) = 0.5;
+        D(0,1) = 0.5;
+        D(N-2,N-1) = 0.5;
+        D(N-2,N-2) = 0.5;
+
+        // unused cell
         D.row(N-1).setZero();
-        D.row(N-2).setZero();
     }
     else
     {
-        ArrayX diff = dzStaggered(L,N);
-        ArrayX diff2 = dz(L,N);
+        // 2nd order interpolation
+        D.diagonal(1).setConstant(0.5);
+        D.diagonal(-1).setConstant(0.5);
 
-        D.diagonal(-1).head(N-2) = 0.5*diff2.tail(N-2)/diff.segment(1, N-2);
-        D.diagonal().segment(1, N-2) = 0.5*diff2.head(N-2)/diff.segment(1, N-2);
+        // values at ends interpolate to give correct answer
+        D.row(0) = -D.row(1);
+        D(0,0) += 2;
 
-        // no derivative at ends
-        D.row(0) = D.row(1);
-        D.row(N-1) = D.row(N-2);
+        D.row(N-1) = -D.row(N-2);
+        D(N-1,N-2) = 2;
     }
 
     return D;
