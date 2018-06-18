2296381f2038534d7356350ff573dfb49d9972da
Jeremy Parker <jpp39@cam.ac.uk>
2018-06-18 08:29:57 +0100

    Track eigenvector if given

--- TrackSolution.cpp
+++ TrackSolution.cpp
@@ -10,18 +10,21 @@ int main(int argc, char* argv[])
     state.LoadFromFile(argv[1]);
     Ri = state.p;
 
-    // load eigenmode
-    StateVector eigenMode;
-    eigenMode.LoadFromFile(argv[2]);
+    if (argc == 3)
+    {
+        // load eigenmode
+        StateVector eigenMode;
+        eigenMode.LoadFromFile(argv[2]);
 
-    stratifloat delta = 0.0001;
+        stratifloat delta = 0.0001;
 
-    // add the eigenmode
-    state.x.MulAdd(delta, eigenMode);
+        // add the eigenmode
+        state.x.MulAdd(delta, eigenMode);
+    }
 
     // follow the trajectory
     ExtendedStateVector result;
-    state.FullEvolve(10000, result, true, true);
+    state.FullEvolve(13000, result, true, true);
 
     result.SaveToFile("result");
 }
