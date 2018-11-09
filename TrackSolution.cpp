a70198e799768f4f35f1093e89b3e3293147eb69
Jeremy Parker <jpp39@cam.ac.uk>
2018-11-09 16:28:30 +0000

    track loaded

--- TrackSolution.cpp
+++ TrackSolution.cpp
@@ -4,15 +4,21 @@
 int main(int argc, char* argv[])
 {
     Ri = std::stod(argv[1]);
-
     DumpParameters();
 
     StateVector state;
-    state.ExciteLowWavenumbers(0.001);
-
-    for (int n=0; n<500; n++)
+    if (argc>2)
     {
-        state.PlotAll(std::to_string(n));
+        state.LoadFromFile(argv[2]);
+    }
+    else
+    {
+        state.ExciteLowWavenumbers(0.001);
+    }
+
+    for (int n=0; n<3000; n++)
+    {
+        //state.PlotAll(std::to_string(n));
 
         std::cout << "Step " << n << " " << state.Energy() << " " << state.Enstrophy() << std::endl;
 
