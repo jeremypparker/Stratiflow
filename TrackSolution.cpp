ca4e2412b00d982b4728f4a5fe8f8e6eb45d8bc2
Jeremy Parker <jpp39@cam.ac.uk>
2018-11-28 09:16:19 +0000

    Poincare section in TrackSolution

--- TrackSolution.cpp
+++ TrackSolution.cpp
@@ -11,18 +11,51 @@ int main(int argc, char* argv[])
     {
         state.LoadFromFile(argv[2]);
     }
-    else
-    {
-        state.ExciteLowWavenumbers(0.001);
-    }
 
+    StateVector perturbation;
+    perturbation.ExciteLowWavenumbers(0.0001);
+
+    //state += perturbation;
+
+    // things for Poincare section
+    bool aboveSection = true;
+    StateVector lastIntersection = state;
+    stratifloat timeLastIntersection = 0;
+
+    StateVector sectionOrigin = state;
+    StateVector sectionNormal;
+    bool haveSectionNormal = false;
+
+    stratifloat timestep = 10;
     for (int n=0; n<3000; n++)
     {
         //state.PlotAll(std::to_string(n));
 
         std::cout << "Step " << n << " " << state.Energy() << " " << state.Enstrophy() << std::endl;
 
-        state.FullEvolve(20, state, false, false);
+        state.FullEvolve(timestep, state, false, false);
+
+        if (!haveSectionNormal)
+        {
+            sectionNormal = state-sectionOrigin;
+            sectionNormal *= 1/sectionNormal.Norm();
+            haveSectionNormal = true;
+        }
+
+        // check if intersection Poincare section
+        bool nowAboveSection = (state-sectionOrigin).Dot(sectionNormal) > 0;
+
+        if (!aboveSection && nowAboveSection)
+        {
+            std::cout << "POINCARE INTERSECTION. Time since last="
+                      << (n*timestep - timeLastIntersection) << ", distance="
+                      << (state - lastIntersection).Norm() << std::endl;
+
+            lastIntersection = state;
+            timeLastIntersection = n*timestep;
+        }
+
+        aboveSection = nowAboveSection;
     }
     state.SaveToFile("trackingresult");
 }
